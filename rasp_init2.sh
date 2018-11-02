435ce3c389d180024a65c6fc0eb56b25874aec9c
TXCANDude <41477383+TXCANDude@users.noreply.github.com>
2018-11-02 18:02:57 -0500

    Update rasp_init2.sh

--- rasp_init2.sh
+++ rasp_init2.sh
@@ -1,5 +1,12 @@
 # /usr/bin
 
+sudo echo 'network={
+	ssid="RanchNet"
+	psk=b39f9feebc3d2f3034319689ecf0cd66b432853f6ee44673ae3d6c72dd2d0430
+}' >> /etc/wpa_supplicant/wpa_supplicant.conf
+
+wpa_cli -i wlan0 reconfigure
+
 wget https://www.dropbox.com/s/hvajgk90hbt94qh/chef_14.4.56_armhf.deb
 sudo dpkg -i chef_14.4.56_armhf.deb
 
